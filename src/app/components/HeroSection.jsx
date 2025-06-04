028e76b520bbf79f80a31c6f729c18ad44a86019
Berry461 <ogunmorotiopeyemi1@gmail.com>
2025-06-04 18:40:43 +0100

    Update Image

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -78,8 +78,8 @@ const HeroSection = () => {
               src="/images/hero-image.jpg"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={400}
-              height={400}
+              width={250}
+              height={250}
             />
           </div>
         </motion.div>
